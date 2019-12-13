a333d247e12e809ae3c8839a5666528fa665f597
PMiskew <paul.miskew@gmail.com>
2019-12-13 10:53:18 -0500

    update

--- Transistion Exercise/dataAnalysis1Video.py
+++ Transistion Exercise/dataAnalysis1Video.py
@@ -5,7 +5,7 @@ data = open("randomDataRAW.txt","r") #Opens the file
 
 
 dataString = data.read() #reads all the content from the file and stores it as a stirng
-#print(dataString)
+print(dataString)
 
 #copy the data from the string into a list
 dataList = dataString.split("\n") #takes the string and converts it to a list of strings using \n as inicator for new element
@@ -18,6 +18,12 @@ dataList = dataString.split("\n") #takes the string and converts it to a list of
 for i in range(0, len(dataList),1): #loops through every element
 	dataList[i] = dataList[i].replace(",","") #replace all the , with ""
 	dataList[i] = float(dataList[i]) #cast the element to a float
+
+
+
+
+
+
 
 
 print(dataList)
--- Transistion Exercise/dataAnalysis2.py
+++ Transistion Exercise/dataAnalysis2.py
@@ -19,7 +19,7 @@ print(dataList)
 #			set the max/min to an element in the list
 
 #Find largest
-max = dataList[0] #always set
+max = dataList[0] #always set to an exisiting element
 
 for i in range(0, len(dataList),1):
 	if dataList[i] > max:
@@ -29,5 +29,21 @@ print(max)
 
 
 #Find smallest
+min = dataList[0] #always set to an existing element
+for i in range(0, len(dataList),1):
+	if dataList[i] < min:
+		min = dataList[i]
+
+print(max)
+
+#Sort a datalist
+dataListsorted = dataList.sort()
+
 #Find mean, median, mode
+streak = 0;
+streakStartIndex = 0;
+
+for i in range(0, len())
+
+
 #Remove outliers (inner quartile range)
