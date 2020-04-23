0c295c5713574e406d010e50742e08372b0fba19
PMiskew <paul.miskew@gmail.com>
2020-04-22 20:17:36 -0400

    update

--- Simple_Game_Example/game_stagethread.py
+++ Simple_Game_Example/game_stagethread.py
@@ -5,85 +5,53 @@ from tkinter import messagebox
 from time import sleep
 
 print("STAGE 2")
+balldata = [0,10,10]
 
-#Binding Source Link
-#https://www.python-course.eu/tkinter_events_binds.php
-def motion(event):
-  print("Mouse position: (%s %s)" % (event.x, event.y))
-  return
+'''
+Only run one method at a time.  They will otherwise conflict. 
+'''
 
-def background_task():
+'''
+def printit():
 
-	xpos = 20
-	ypos = 200
-	while True:
-		canvas.create_oval(xpos,ypos,xpos+20,ypos+20,fill = "black")
+	#Change the 1.0 to reset the time interval that the function is called. 
+	threading.Timer(1.0, printit).start()
+	print ("Hello, World!")
+	balldata[0] = balldata[0] + 1
+	if balldata[0] % 2 == 0:
+		canvas.create_oval(balldata[1],balldata[2],balldata[1] + 10,balldata[2] + 10, fill = "black",outline = "white")
+	else:
+		canvas.create_oval(balldata[1],balldata[2],balldata[1] + 10,balldata[2] + 10, fill = "white", outline = "white")
+
+	print(balldata[0])
+#'''
+
+def printit():
+
+	#Change the 1.0 to reset the time interval that the function is called. 
+	threading.Timer(0.25, printit).start()
+	print ("Hello, World!")
+	
+	
+	canvas.create_oval(balldata[1],balldata[2],balldata[1] + 10,balldata[2] + 10, fill = "black",outline = "white")
+	canvas.create_oval(balldata[1],balldata[2],balldata[1] + 10,balldata[2] + 10, fill = "white", outline = "white")
 		
-		canvas.create_oval(xpos,ypos,xpos+20,ypos+20,fill = "white")
-		
-		
-		
+	canvas.create_oval(balldata[1],balldata[2],balldata[1] + 10,balldata[2] + 10, fill = "black",outline = "white")
+	balldata[1] = balldata[1] + 1
+	balldata[2] = balldata[2] + 1
+	print(balldata[0])
 
 
-
-
-def on_closing():
-	if messagebox.askokcancel("Quit", "Do you want to quit?"):
-		root.destroy()
-
-
-
-root = tk.Tk()
-
-#https://effbot.org/tkinterbook/canvas.htm
+root = tk.Tk()	
 canvas = tk.Canvas(root, width=300, height=300)
 canvas.pack()
 
-#Create image
-#Task:  Using the documentation found at the below link
-#	https://tkdocs.com/tutorial/canvas.html
-# 	10 minutes - create a picture for me. 
 
 
-canvas.create_rectangle(132,110,173,160, fill = "grey") #neck
-canvas.create_oval(100,30, 200,130, fill = "grey") #head
+printit()
 
 
-fontStyle = tkFont.Font(family="Lucida Grande", size=50)
-canvas.create_text(207, 100, text = "m", angle=270, font = fontStyle)
 
-#use variables to change position quickly and maintain relationships
-hx = 160
-hy = 50
-hr = 25
-canvas.create_oval(hx,hy,hx+hr,hy+hr, fill = "white") #eye
-
-canvas.create_oval(172,60,180,68,fill = "black") #pupil
-
-#This creates a grid.  By using the loop we can shrink or exand the size
-#Caution:	Remember loops take over the processor so while it is running
-#			nothing else can happen.  This is often how a student first 
-#			tries to create animations.  
-#			Though this works the problem is nothign else can happen, such
-#			as clicking
-for i in range(0,40,3):
-
-	canvas.create_line(0,0 + i,40,0 + i)
-	canvas.create_line(0 + i,0,0 + i,40)
-
-
-canvas.bind('<Motion>',motion) #bind a mouse motion listener
-
-block_pos = canvas.coords(block.id)
-console.log(blockpos);
-
-#Creates Thread
-#What is a thread?
-#t = threading.Thread(target=background_task)
-#t.start() #starts thread
-
-
-root.protocol("WM_DELETE_WINDOW", on_closing)
 root.mainloop();
 
 
