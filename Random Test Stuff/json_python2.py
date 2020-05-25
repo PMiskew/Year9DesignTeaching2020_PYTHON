b425b12bbcbe684c1db206729b2db0bd3e6e1543
PMiskew <paul.miskew@gmail.com>
2020-05-25 08:56:40 -0400

    update

---
+++ Random Test Stuff/json_python2.py
@@ -0,0 +1,11 @@
+#https://realpython.com/python-json/
+#https://jsonplaceholder.typicode.com/posts
+#
+
+import json
+import requests
+
+response = requests.get("https://jsonplaceholder.typicode.com/posts")
+todos = json.loads(response.text)
+
+print(todos[0]["userId"])
